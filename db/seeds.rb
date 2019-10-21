9de3ea3a38c698fcb2e146403fc532a149953168
github username <github email address>
2019-10-21 10:48:53 +0100

    Seed data added

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,21 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
+User.create(username: 'Sarah', password_digest: "password1", email: 'one@gmail.com' )
+User.create(username: 'Ilita', password_digest: "password2", email: 'two@gmail.com' )
+
+Seller.create(name: 'freshFields', email: 'fields@gmaill.com', image: 'fields' )
+Seller.create(name: 'Pickle', email: 'pickle@gmaill.com', image: 'sour' )
+
+Product.create(name: 'Beetroot', price: 2, image: 'beetroot', seller_id: Seller.first.id)
+Product.create(name: 'cucumber', price: 1, image: 'cucmber', seller_id: Seller.last.id)
+
+Order.create(user_id: User.first.id, total: 0)
+Order.create(user_id: User.last.id, total: 0)
+
+
+OrderItem.create( order_id: Order.first.id, product_id: Product.first.id)
+OrderItem.create( order_id: Order.last.id, product_id: Product.last.id)
+
+
+
