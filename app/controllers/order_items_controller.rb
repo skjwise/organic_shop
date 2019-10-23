59bff4584ec7656b95a7932005a4330ac771af30
github username <github email address>
2019-10-23 19:11:54 +0100

    priceAdded

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -13,6 +13,30 @@ class OrderItemsController < ApplicationController
     params.require(:order_items).permit!
    end
 
+   def create
+    @order = current_order
+    # @order_item = @order.order_items.new(order_item_params)
+    # @order.save
+    # session[:order_id] = @order.id
+
+    @current_item = @order.order_items.find_by(product_id: params[:order_item][:product_id])
+    p "current item =", @current_item
+
+    if @current_item
+      @current_item.quantity += 1
+      @current_item.save
+      #udpate subtotal
+      @order.update_total
+
+      session[:order_id] = @current_item.order_id
+    else
+      @order_item = @order.order_items.new(order_item_params)
+      @order_item.save
+      # @order.save
+      @order.update_total
+      session[:order_id] = @order.id
+    end
+  end
 end
 
 
--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,8 @@ class Product < ApplicationRecord
     belongs_to :seller
     has_many :order_items
     has_many :orders, through: :order_items
+
+    def quid(price)
+        number_to_currency(price, :unit => "£")
+      end
 end
