42ec53a3e589ded28d59949a63cd8a0b4a51f559
github username <github email address>
2019-10-23 17:20:01 +0100

    cartviewimplemented

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -1,24 +1,18 @@
 class OrderItemsController < ApplicationController
-
   def show
     @order_items = current_cart.map{|id| Product.find(id)}.uniq
   end
 
 
   def destroy
-    # item = session[:cart].map{|id| Product.find(id)}
-    # byebug
-    # #  @item.destroy
-    # #  item.save
-    # # redirect_to order_item_path
-
     session[:cart].delete(params[:id].to_i)
-
-    redirect_to order_item_path(current_cart)
+      redirect_to sellers_path
   end
+
    def order_items_params
     params.require(:order_items).permit!
    end
+
 end
 
 
--- app/models/seller.rb
+++ app/models/seller.rb
@@ -1,3 +1,7 @@
 class Seller < ApplicationRecord
     has_many :products
+
+    def seller_products
+        self.products
+    end
 end
