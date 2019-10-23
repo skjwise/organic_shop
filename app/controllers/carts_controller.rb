8e353ddb2aebc739400158a2b350fd58d539260f
github username <github email address>
2019-10-23 12:10:05 +0100

    deletesessionitem

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,11 +1,11 @@
 class CartsController < ApplicationController
   def index
-    @order_items = current_cart.map{|id| Product.find(id)}.map{|product| product.name}.uniq
+    @order_items = current_cart.map{|id| Product.find(id)}
   end
 
   def show
-    @order_items = current_cart.order_items
-   
+    # @order_items = current_cart.order_items
+    @order_items = current_cart.map{|id| Product.find(id)}
   end
 
 
@@ -18,6 +18,7 @@ class CartsController < ApplicationController
   def add_to_cart
     # @product = Product.find(params[:product_id])
     if current_cart << params[:product_id].to_i
+      byebug
       redirect_to order_item_path(current_cart)
     # byebug
   end
--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -1,4 +1,21 @@
 class OrderItemsController < ApplicationController
+
   def show
+    @order_items = current_cart.map{|id| Product.find(id)}.uniq
   end
+
+
+  def destroy
+    item = session[:cart].map{|id| Product.find(id)}
+    byebug
+     @item.destroy
+     item.save
+    redirect_to order_item_path
+   end
+
+   def order_items_parans
+    params.require(:order_items).permit!
+   end
 end
+
+
