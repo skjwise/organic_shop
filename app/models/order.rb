81044f4747235b618325967499ee0bfe3255450b
skjwise <skjwise@hotmail.com>
2019-10-23 09:47:52 +0100

    order price methods added

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,25 @@ class Order < ApplicationRecord
     belongs_to :user
     has_many :order_items
     has_many :products, through: :order_items
+    before_save :update_total
+    before_create :update_status
+
+
+    def total_price
+        self.order_items.map {|product| product.price }.sum
+    end
+
+    private 
+
+    def update_status
+        if self.status == nil
+            self.status = "In Progress"
+        else
+            self.status = "Completed!"
+        end
+    end
+
+    def update_total
+        self.total_price = total_price
+    end
 end
