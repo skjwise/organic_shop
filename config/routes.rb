cb178716f8b73a61fb9459b1f41da4c44cdf87d7
github username <github email address>
2019-10-23 18:00:17 +0100

    cartRemoveFunction

--- config/routes.rb
+++ config/routes.rb
@@ -6,20 +6,19 @@ Rails.application.routes.draw do
   resources :orders
   resources :products
   resources :order_items
-  resources :carts
+  # resources :carts
   # resources :sessions
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
   get '/logout', to: 'sessions#destroy'
 
-  post '/add', to: 'orders#add'
   delete '/remove/:id', to: 'order_items#destroy', as: 'remove'
 
   root to: 'sellers#index'
 
-  post '/add_to_cart', to: 'carts#add_to_cart', as: 'add_path'
-  get '/cart', to: 'carts#show', as: 'cart_path'
+  post '/add_to_cart', to: 'carts#add_to_cart', as: 'add'
+  get '/cart', to: 'carts#show', as: 'cart'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
