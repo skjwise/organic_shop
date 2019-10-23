3b5b0246b247c087743807720eb18eca3aec5250
skjwise <skjwise@hotmail.com>
2019-10-23 16:40:40 +0100

    updated navbar

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
+  get '/logout', to: 'sessions#destroy'
 
   post '/add', to: 'orders#add'
   delete '/remove/:id', to: 'order_items#destroy', as: 'remove'
