f20647f8174067cdce8b3899c5d895d2fc264ca3
github username <github email address>
2019-10-23 09:04:53 +0100

    Navbar added

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
 
-  root to: 'organic#index'
+  root to: 'organic#index', to: 'organic#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
