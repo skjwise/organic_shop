094ff912139d2c6538f7c95e57906e6fdf8d91df
github username <github email address>
2019-10-24 09:49:43 +0100

    QuantityaddedTotable

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191023154750) do
+ActiveRecord::Schema.define(version: 20191024074337) do
 
   create_table "order_items", force: :cascade do |t|
     t.integer "order_id"
     t.integer "product_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "quantity"
   end
 
   create_table "orders", force: :cascade do |t|
