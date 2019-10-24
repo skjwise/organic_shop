a63304f2ea0d4950ac5aed26c4f467ca51185617
github username <github email address>
2019-10-24 15:29:01 +0100

    Checkoutmethod

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -5,8 +5,8 @@ class OrderItem < ApplicationRecord
     # validates :quantity, presence: true, numericality: { only_integer: true, greater_than: 0}
     # before_save :update_total
     # before_create :update_status
-    validate :product_present
-    validate :order_present
+    # validate :product_present
+    # validate :order_present
 
     def total_price
         total = order.quantity * order.product.price
