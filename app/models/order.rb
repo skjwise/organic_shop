7cf3e860d25d42be0c6cb8484a499dcbe12397e0
skjwise <skjwise@hotmail.com>
2019-10-24 09:50:51 +0100

    order total first commit

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,25 +2,5 @@ class Order < ApplicationRecord
     belongs_to :user
     has_many :order_items
     has_many :products, through: :order_items
-    before_save :update_total
-    before_create :update_status
-
-
-    def total_price
-        self.order_items.map {|item| item.products.price }.sum
-    end
-
-    private 
-
-    def update_status
-        if self.status == nil
-            self.status = "In Progress"
-        else
-            self.status = "Completed!"
-        end
-    end
-
-    def update_total
-        self.total_price = total_price
-    end
+    
 end
--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,4 +1,41 @@
 class OrderItem < ApplicationRecord
     belongs_to :product
     belongs_to :order
+
+    # validates :quantity, presence: true, numericality: { only_integer: true, greater_than: 0}
+    # before_save :update_total
+    # before_create :update_status
+    validate :product_present
+    validate :order_present
+
+    def total_price
+        self.order_items.map {|item| item.products.price * item.quantity }.sum
+    end
+
+    private 
+
+    # def update_status
+    #     if self.status == nil
+    #         self.status = "In Progress"
+    #     else
+    #         self.status = "Completed!"
+    #     end
+    # end
+
+    def product_present
+        if product.nil?
+            errors.add(:product, "is not valid.")
+        end 
+    end 
+
+    def order_present
+        if order.nil?
+            errors.add(:order, "is not a valid order.")
+        end
+    end
+
+    def update_total
+        self.total_price = total_price
+    end
+
 end
