261241aa29ab40b3b8053c24a1731fa2f26685b6
skjwise <skjwise@hotmail.com>
2019-10-25 13:26:29 +0100

    redirect methods added

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,8 @@
 class ProductsController < ApplicationController
   def index
-    @products = Product.all
-    @oder_item = current_cart.order_items.new
+    # @products = Product.all
+    # @oder_item = current_cart.order_items.new
+    redirect_to sellers_path
   end
 
   def show
