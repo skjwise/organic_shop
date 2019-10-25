0feb15664ad2584fd283289ce34e241f9320e32e
skjwise <skjwise@hotmail.com>
2019-10-25 12:02:34 +0100

    edit profile link added to navbar

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
   delete '/logout', to: 'sessions#destroy'
   get '/logout', to: 'sessions#destroy'
 
+
   delete '/remove/:id', to: 'order_items#destroy', as: 'remove'
 
   root to: 'sellers#index'
